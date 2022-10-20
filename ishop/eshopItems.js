423c4789a603725a9f851cc7760b1ff132f61447
vladklen <Vlad6097944@gmail.com>
2022-10-21 00:08:01 +0300

    Task1

--- ishop/eshopItems.js
+++ ishop/eshopItems.js
@@ -1,6 +1,6 @@
 var eshopItems = React.createClass({
 
-	displayName: 'eshop-item',
+	displayName: 'Eshop-item',
 
 	propTypes: {
 		title: React.PropTypes.string.isRequired, // текст вопроса
@@ -24,23 +24,22 @@ var eshopItems = React.createClass({
 					React.DOM.tr({ className: 'Item__row' },
 						React.DOM.td({ className: 'Item__image', rowSpan: "4" },
 							React.DOM.img({ className: 'photo', src: elem.img })),
-						React.DOM.td({ className: 'Item__name' }, elem.name),
+						React.DOM.td({ className: 'Item__brand' }, elem.brand),
 					),
 					React.DOM.tr({ className: 'Item__row' },
-						React.DOM.td({ className: 'Item__brand' }, `Бренд:${elem.brand}`),
+						React.DOM.td({ className: 'Item__name' }, elem.name),
 					),
 					React.DOM.tr({ className: 'Item__row' },
 						React.DOM.td({ className: 'Item__left' }, `В наличии:${elem.countLeft} шт`),
 					),
 					React.DOM.tr({ className: 'Item__row' },
-						React.DOM.td({ className: 'Item__price' }, `Стоимость:${Math.floor(elem.price * 100) / 100} BYN`),
+						React.DOM.td({ className: 'Item__price' }, `Стоимость:${elem.price.toFixed(2)} BYN`),
 					),
 				)
-
 			),
 		);
-		return React.DOM.div({ className: 'eshop-item' },
-			React.DOM.div({ className: 'Title' }, this.props.title),
+		return React.DOM.div({ className: 'Eshop-item' },
+			React.DOM.h1({ className: 'Title' }, this.props.title),
 			React.DOM.div({ className: 'Catalog' }, eshopItem),
 		);
 	},
