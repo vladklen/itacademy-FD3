1397871d228f1fde9fa129eb8986da630a340f6c
vladklen <Vlad6097944@gmail.com>
2022-11-10 01:18:12 +0300

    Task4

---
+++ RainbowFrame/components/RainbowBlock.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import RainbowFrame from './RainbowFrame';
+import './RainbowBlock.css';
+
+
+class RainbowBlock extends React.Component {
+
+	state = {
+	};
+
+
+	render() {
+		return (
+			<RainbowFrame colors={this.props.colors}>
+				Hello!
+			</RainbowFrame>
+		);
+	}
+}
+
+export default RainbowBlock;
