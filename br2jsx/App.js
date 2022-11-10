8597d05fb25f10be4832e2dcd36197ab86c99598
vladklen <Vlad6097944@gmail.com>
2022-11-10 23:49:12 +0300

    Task5

---
+++ br2jsx/App.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainBlock from './components/MainBlock';
+
+let text = "первый<br>второй<br/>третий<br />последний";
+
+ReactDOM.render(
+	<MainBlock
+		text={text}
+	/>
+	, document.getElementById('container')
+);
+
