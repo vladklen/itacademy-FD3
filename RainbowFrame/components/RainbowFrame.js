72c2c5a546dbd10e592bfb3aa92288b9ea277121
vladklen <Vlad6097944@gmail.com>
2022-11-11 21:18:26 +0300

    Task4

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -9,11 +9,10 @@ class RainbowFrame extends React.Component {
 	render() {
 		console.log(this.props.colors)
 
-		let test = this.props.colors.reduce((acc, el, index) => {
+		let test = this.props.colors.reduce((acc, el) => {
 			return <div style={{ border: "solid 2px " + el, padding: "20px" }} >{acc}</div>
 		}, this.props.children)
 
-		console.log(test)
 
 		return (
 			<div>{test}</div>
