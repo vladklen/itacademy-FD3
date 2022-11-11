f3cf9a06976454c1dfaa1ace832329f19635a9b1
vladklen <Vlad6097944@gmail.com>
2022-11-11 23:37:43 +0300

    Task6

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 
 let withRainbowFrame = color => Comp => props => {
-	let test = color.reduce((acc, el) => {
+	let colorBorder = color.reduce((acc, el) => {
 		return <div style={{ border: "solid 2px " + el, padding: "5px" }} >{acc}</div>
 	}, <Comp {...props} />)
 
 
 	return (
-		<div>{test}</div>
+		<div>{colorBorder}</div>
 	);
 }
 
